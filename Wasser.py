280ae47cde656a05f2456846d8175434c8e3c977
Adaskox <adam.martka@gmail.com>
2023-05-30 05:19:36 +0200

    -Made so you can't color more than 14 rectangles

--- Wasser.py
+++ Wasser.py
@@ -6,16 +6,23 @@ customtkinter.set_default_color_theme('dark-blue')
 
 # Box
 root = customtkinter.CTk()
-root.geometry('500x800')
+root.geometry('500x750')
 
 # Function inside it
 def add_water():
-    active_rectangle[0] = (active_rectangle[0] - 1) % len(rectangles)
-    fill_color = canvas.itemcget(rectangles[active_rectangle[0]], 'fill')
-    if fill_color == 'grey':
+    if canvas.itemcget(rectangles[active_rectangle[0]], 'fill') == 'grey':
         canvas.itemconfig(rectangles[active_rectangle[0]], fill='blue')
+        active_rectangle[0] = (active_rectangle[0] - 1) % len(rectangles)
+    elif all(canvas.itemcget(rectangle, 'fill') == 'blue' for rectangle in rectangles):
+        # All rectangles are already blue, do nothing
+        pass
     else:
-        canvas.itemconfig(rectangles[active_rectangle[0]], fill='grey')
+        # Find the first grey rectangle and color it blue
+        for rectangle in rectangles:
+            if canvas.itemcget(rectangle, 'fill') == 'grey':
+                canvas.itemconfig(rectangle, fill='blue')
+                active_rectangle[0] = (active_rectangle[0] - 1) % len(rectangles)
+                break
 
 frame = customtkinter.CTkFrame(master=root)
 frame.pack(pady=20, padx=60, fill='both', expand=True)
@@ -24,18 +31,18 @@ frame.pack(pady=20, padx=60, fill='both', expand=True)
 label = customtkinter.CTkLabel(master=frame, text='Wasser', font=('Arial', 24))
 label.pack(pady=12, padx=10)
 
-canvas = customtkinter.CTkCanvas(root, width=200, height=500)
+canvas = customtkinter.CTkCanvas(root, width=200, height=400)
 canvas.pack(pady=20)
 
 # Split canvas into 14 rectangles - 250ml of water each
 rectangles = []
 for i in range(14):
-    y1 = i * 350 // 14
-    y2 = (i + 1) * 350 // 14
+    y1 = (i + 1) * 350 // 14
+    y2 = (i + 2) * 350 // 14
     rectangle = canvas.create_rectangle(0, y1, 200, y2, fill='grey')
     rectangles.append(rectangle)
 
-active_rectangle = [len(rectangles) - 0]  # Start from the last rectangle index
+active_rectangle = [len(rectangles) - 1]  # Start from the third last rectangle index
 
 button = customtkinter.CTkButton(master=frame, text='Add Water', command=add_water)
 button.pack(pady=12, padx=10)
